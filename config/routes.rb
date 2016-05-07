5f4a2b02e2c0cfaaa62d43e6c9581ee7ec7e35c8
= <=>
2016-05-07 14:24:29 -0600

    New MacBook

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   #get 'welcome/index'
 
   resources :articles
@@ -17,7 +18,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'articles#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
