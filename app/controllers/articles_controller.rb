d98e737f451a9aefca95c8c933053d61ac8912f1
Kev <kevynquiros@gmail.com>
2016-05-12 09:32:16 -0600

    Added coment scaffold to app, refactoring...

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,12 @@
 class ArticlesController < ApplicationController
+  before_action :authenticate_user!, except: [:show, :index]
+  before_action :set_article, except: [:new, :index, :create]
   def index
   	@articles = Article.all
   end
 
   def show
-  	@article = Article.find(params[:id])
+    @article.update_visits_count
   end
 
   def new
@@ -23,7 +25,6 @@ class ArticlesController < ApplicationController
   end
 
   def update
-  	@article = Article.find(params[:id])
   	if @article.update(article_params)
   		redirect_to @article
   	else
@@ -32,16 +33,18 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-  	@article = Article.find(params[:id])
   end
 
   def destroy
-  	@article = Article.find(params[:id])
   	@article.destroy
   	redirect_to articles_path
   end
 
   private
+
+  def set_article
+  	@article = Article.find(params[:id])
+  end
 
   def article_params
   	params.require(:article).permit(:title, :body)
--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,17 @@ class Article < ActiveRecord::Base
 	belongs_to :user
 	validates :title, presence: true
 	validates :body, presence: true
+	before_create :set_visits_count
+
+	def set_visits_count
+		self.visits_count = 0
+	end
+
+	def update_visits_count
+		self.set_visits_count if self.visits_count.nil?
+		#if current_user != self.user
+			self.update(visits_count: self.visits_count + 1)
+		#end
+	end
+
 end
